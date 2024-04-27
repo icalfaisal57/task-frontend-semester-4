1752cb194d48318b8d8cacab1e5c44aa9bb6d7db
icalfaisal57 <icalfaisal57@gmail.com>
2024-04-27 10:58:15 +0700

    pertemuan dan task 6

--- task react/movie-app/src/components/Movies/Movies.jsx
+++ task react/movie-app/src/components/Movies/Movies.jsx
@@ -1,101 +1,33 @@
 import styles from "./Movies.module.css";
+import Movie from "../Movie/Movie";
+import data from "../../utils/constants/data";
+import { useState } from "react";
 function Movies() {
+	const [movies, setMovie] = useState(data);
+	
+	function handleClick(){
+		const newMovie = {
+			id: "tt10872600",
+			title: "Spider-Man",
+			year: "2021",
+			type: "Movie",
+			poster:
+				"https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_SX300.jpg",
+		};
+		setMovie([...movies,newMovie])
+	}
+
 	return (
 		<div className={styles.container}>
 			<section className={styles.movies}>
 				<h2 className={styles.movies__title}>Latest Movies</h2>
 				<div className={styles.movie__container}>
-					<div className={styles.movie}>
-						<img
-							className={styles.movie__image}
-							src="https://picsum.photos/536/354"
-							alt="ini poster film"
-						/>
-						<h3 className={styles.movie__title}>Title</h3>
-						<p className={styles.movie__date}>Date Titke</p>
-					</div>
-					<div className={styles.movie}>
-						<img
-							className={styles.movie__image}
-							src="https://picsum.photos/536/354"
-							alt="ini poster film"
-						/>
-						<h3 className={styles.movie__title}>Title</h3>
-						<p className={styles.movie__date}>Date Titke</p>
-					</div>
-					<div className={styles.movie}>
-						<img
-							className={styles.movie__image}
-							src="https://picsum.photos/536/354"
-							alt="ini poster film"
-						/>
-						<h3 className={styles.movie__title}>Title</h3>
-						<p className={styles.movie__date}>Date Titke</p>
-					</div>
-					<div className={styles.movie}>
-						<img
-							className={styles.movie__image}
-							src="https://picsum.photos/536/354"
-							alt="ini poster film"
-						/>
-						<h3 className={styles.movie__title}>Title</h3>
-						<p className={styles.movie__date}>Date Titke</p>
-					</div>
-					<div className={styles.movie}>
-						<img
-							className={styles.movie__image}
-							src="https://picsum.photos/536/354"
-							alt="ini poster film"
-						/>
-						<h3 className={styles.movie__title}>Title</h3>
-						<p className={styles.movie__date}>Date Titke</p>
-					</div>
-					<div className={styles.movie}>
-						<img
-							className={styles.movie__image}
-							src="https://picsum.photos/536/354"
-							alt="ini poster film"
-						/>
-						<h3 className={styles.movie__title}>Title</h3>
-						<p className={styles.movie__date}>Date Titke</p>
-					</div>
-					<div className={styles.movie}>
-						<img
-							className={styles.movie__image}
-							src="https://picsum.photos/536/354"
-							alt="ini poster film"
-						/>
-						<h3 className={styles.movie__title}>Title</h3>
-						<p className={styles.movie__date}>Date Titke</p>
-					</div>
-					<div className={styles.movie}>
-						<img
-							className={styles.movie__image}
-							src="https://picsum.photos/536/354"
-							alt="ini poster film"
-						/>
-						<h3 className={styles.movie__title}>Title</h3>
-						<p className={styles.movie__date}>Date Titke</p>
-					</div>
-					<div className={styles.movie}>
-						<img
-							className={styles.movie__image}
-							src="https://picsum.photos/536/354"
-							alt="ini poster film"
-						/>
-						<h3 className={styles.movie__title}>Title</h3>
-						<p className={styles.movie__date}>Date Titke</p>
-					</div>
-					<div className={styles.movie}>
-						<img
-							className={styles.movie__image}
-							src="https://picsum.photos/536/354"
-							alt="ini poster film"
-						/>
-						<h3 className={styles.movie__title}>Title</h3>
-						<p className={styles.movie__date}>Date Titke</p>
-					</div>
+					{movies.map((movie)=>{
+						return <Movie key={movie.id} movie={movie} />;
+					})}
+					<button onClick={handleClick}>submit</button>
 				</div>
+				
 			</section>
 		</div>
 	);
--- task react/movie-app/src/components/Navbar/Navbar.jsx
+++ task react/movie-app/src/components/Navbar/Navbar.jsx
@@ -6,9 +6,9 @@ function Navbar() {
 			<nav className={styles.navbar}>
 				<h2 className={styles.navbar__brand}>Movie App</h2>
 				<ul className={styles.navbar__list}>
-					<li className={styles.navbar__item}>home</li>
-					<li className={styles.navbar__item}>home</li>
-					<li className={styles.navbar__item}>home</li>
+					<li className={styles.navbar__item}>Home</li>
+					<li className={styles.navbar__item}>Menu</li>
+					<li className={styles.navbar__item}>Search</li>
 				</ul>
 			</nav>
 		</div>
